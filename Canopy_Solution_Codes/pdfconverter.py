cac1beffb6d2581d552d7fd09fd22c8d9b950c67
Uma-kanagarajan <39462776+Uma-kanagarajan@users.noreply.github.com>
2018-05-21 03:52:18 +0800

    Solution for the given technical task

--- Canopy_Solution_Codes/pdfconverter.py
+++ Canopy_Solution_Codes/pdfconverter.py
@@ -6,9 +6,3 @@ for filename in glob.glob('*.pdf'):
     excel_convert = pdftables_api.Client('8nehrotcon8b') #if needed use your API key
     excel_convert.xlsx(filename, excel_filename)
 
-
-
-
-
-#c = pdftables_api.Client('8nehrotcon8b')
-#c.xlsx('Bos.pdf', 'Bos_excel') #replace c.xlsx with c.csv to convert to CSV
